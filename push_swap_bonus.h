02ea0e7780979a75f6b4dacf0c1a825f0b5ad556
Lucca Cruz <lucca1609.ls@gmail.com>
2025-06-27 19:16:42 +0100

    fix: uppercase OK and KO macro values.

--- push_swap_bonus.h
+++ push_swap_bonus.h
@@ -6,15 +6,15 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 17:48:35 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/06/27 18:48:17 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/06/27 19:03:24 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_BONUS_H
 # define PUSH_SWAP_BONUS_H
 
-# define OK "ok\n"
-# define KO "ko\n"
+# define OK "OK\n"
+# define KO "KO\n"
 
 # include "push_swap.h"
 
