71e34f829dada9074817c4edef5733e8736a343c
Lucca Cruz <lucca1609.ls@gmail.com>
2025-07-01 21:00:49 +0100

    feat: check if the stack is already sorted (do nothing if it is).

--- main.c
+++ main.c
@@ -6,12 +6,27 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 15:09:14 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/07/01 15:33:46 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/07/01 20:54:07 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h>
+#include <stdbool.h>
+
+static bool	is_sorted(t_stack *a)
+{
+	int	i;
+
+	i = 0;
+	while (i < a->len - 1)
+	{
+		if (a->arr[i] > a->arr[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,6 +37,8 @@ int	main(int argc, char **argv)
 	{
 		if (!init_stacks(&a, &b, (argv + 1), arrlen(argv + 1)))
 			return (1);
+		if (is_sorted(&a))
+			return (0);
 		else if (a.len == 2 && a.arr[0] > a.arr[1])
 			sa(&a);
 		else if (a.len == 3)
